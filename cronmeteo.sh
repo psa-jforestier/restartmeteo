52720278271b4b0b0a51fdcd365d7eefaab995f1
psa-jforestier <jerome@jejepi>
2026-09-24 07:28:51 +0200

    fix path

--- cronmeteo.sh
+++ cronmeteo.sh
@@ -19,18 +19,24 @@ python3 sm_forecast.py --backend file $STARMETEO_TMP --days 5 --output txt
 ## Generate forcast for starmeteo
 prev=$(python3 sm_forecast.py --backend file $STARMETEO_TMP --output starmeteo)
 sm_format=$(starmeteo $prev -areaid:$STARMETEO_AREA -quiet -rpitx)
-echo -n "$sm_format" | sudo ./pocsag -f "$STARMETEO_FREQ" -r 1200 -t 1 2>/dev/null
+
+## Send a first forecast
+echo "forecast=$sm_format"
+echo -n "$sm_format" | sudo pocsag -f "$STARMETEO_FREQ" -r 1200 -t 1 2>/dev/null
 sleep 4
+
 # Time sync
 echo "Time sync on $(date)"
 curtime=$(starmeteo -time -areas:$STARMETEO_AREA -quiet -rpitx)
 echo " curtime=$curtime"
-echo -n "$curtime" | sudo ./pocsag -f "$STARMETEO_FREQ" -r 1200 -t 1 2>/dev/null
+echo -n "$curtime" | sudo pocsag -f "$STARMETEO_FREQ" -r 1200 -t 1 2>/dev/null
+sleep 4
+
+echo "Forecast again"
+# Forcast again
+echo -n "$sm_format" | sudo pocsag -f "$STARMETEO_FREQ" -r 1200 -t 1 2>/dev/null
 sleep 4
 
 # Forcast again
-echo " forecast=$sm_format"
-echo -n "$sm_format" | sudo ./pocsag -f "$STARMETEO_FREQ" -r 1200 -t 1 2>/dev/null
-sleep 4
-echo -n "$sm_format" | sudo ./pocsag -f "$STARMETEO_FREQ" -r 1200 -t 1 2>/dev/null
+echo -n "$sm_format" | sudo pocsag -f "$STARMETEO_FREQ" -r 1200 -t 1 2>/dev/null
 
